c7262b6b8d3fa14c1a416e23b5f68e396410fe5b
Reynardo Tjhin <reychen26@gmail.com>
2026-03-02 23:51:32 +1100

    init create post

---
+++ flask-bootstrap-sqlite3/src/static/blog/createPost.js
@@ -0,0 +1,41 @@
+document.getElementById("createPostForm").addEventListener('submit', async function (event) {
+    event.preventDefault(); // prevent from normal submission
+
+    // disable submit button to prevent double submission
+    document.getElementById("createPostButton").disabled = true;
+
+    // get the post details
+    const form = event.target;
+    const formData = new FormData(form);
+    
+    // turn it into JSON string
+    const obj = {};
+    formData.forEach((value, key) => {
+        obj[key] = value;
+    });
+    const json = JSON.stringify(obj);
+
+    // perform an async request
+    try {
+        const csrfToken = document.getElementById("createPostCSRFToken").value;
+        console.log(csrfToken);
+
+        const resp = await fetch("/api/v1/blog/posts", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken,
+            },
+            body: json,
+        });
+        const res = await resp.json();
+
+        console.log(res);
+        if (res.message === 'successfully added a new post') {
+            window.location.href = '/';
+        }
+
+    } catch (error) {
+        console.error("Error in '': " + error);
+    }
+});
